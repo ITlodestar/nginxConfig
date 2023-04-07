a1f7f824669cb3c66a312b3cb73b7fced3f88632
ITlodestar <maksymberezhnoy95@gmail.com>
2023-04-07 18:38:12 +0600

    echo help

--- database/config/start.php
+++ database/config/start.php
@@ -74,7 +74,8 @@ foreach ($data as $fetch) {
 			file_put_contents("/etc/nginx/sites-enabled/$d.conf", $tmpl);
 			echo "Domain $d is successful\n";
 		} else {
-			echo "Domain $d is failed\n$cmd\n$exist_str\n$outArr\n$result\n";
+			echo "Domain $d is failed\n$cmd\n$exist_str\n$result\n";
+			var_dump($outArr);
 		}
 	} else {
 		echo "Domain $d is not pointed to $serverIP\n";
